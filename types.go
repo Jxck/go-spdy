2c29180544ee130d401083d44546a7c08ca3e9d1
Jxck <block.rxckin.beats@gmail.com>
2013-01-21 00:52:08 +0900

    update package doc

--- types.go
+++ types.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package spdy implements SPDY/3 protocol which is described in
+// Package spdy implements the SPDY protocol (currently SPDY/3), described in
 // http://www.chromium.org/spdy/spdy-protocol/spdy-protocol-draft3
 package spdy
 
